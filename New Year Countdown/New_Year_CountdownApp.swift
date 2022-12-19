fc48b2cfb1299fce164290ec4783e17f7012f22b
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-12-19 19:55:09 +0300

    Initial Commit

---
+++ New Year Countdown/New_Year_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Year_CountdownApp.swift
+//  New Year Countdown
+//
+//  Created by Anatoliy on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct New_Year_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
